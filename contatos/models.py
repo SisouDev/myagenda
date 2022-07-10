d181e0ef92c9ff8cb96778359d9df1a4b26f7d18
Maria Karolina Silva de Souza <karolinasingulani2@gmail.com>
2022-07-10 00:52:04 -0300

    Agenda

---
+++ contatos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Sexo(models.Model):
+    sexo = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.sexo
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    sexo = models.ForeignKey(Sexo, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m')
+
+    def __str__(self):
+        return self.nome
